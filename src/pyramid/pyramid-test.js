6f52aeb70ded75e92611c57ca06457d45d3ed40d
Walker Malling <walker.malling@gmail.com>
2014-12-30 09:48:10 -0800

    add tests, correct parent assignment'

--- src/pyramid/pyramid-test.js
+++ src/pyramid/pyramid-test.js
@@ -8,17 +8,36 @@ describe('Pyramid',function(){
 
   var p;
 
-  beforeEach(function(){
+  beforeEach(function (){
     p = new Pyramid();
   });
 
-  it('doesn\'t suck', function(){
+  it('doesn\'t suck', function (){
     expect(p).to.be.instanceOf(Pyramid);
   });
 
-  it('should generate a pyramid from an array of numbers');
+  it('should generate a pyramid from an array of numbers', function () {
+    p.generateFromArray([1,2,3,4,5,6]);
+    expect(p.inserted).to.eql(6);
+  });
 
-  it('should genearte a pyramid from an array of arrays of numbers');
+  it('should genearte a pyramid from an array of arrays of numbers', function () {
+    p.generateFromArray([
+        [1],
+        [2,3],
+        [4,5,6]
+      ]);
+    expect(p.inserted).to.eql(6);
+  });
+
+  it('nodes with two parents should have two parents', function () {
+    p.generateFromArray([1,2,3,4,5,6]);
+    var fifthNode = p.root.leftChild.rightChild;
+    expect(fifthNode.data).to.eql(5);
+    expect(fifthNode.leftParent.data).to.eql(2);
+    expect(fifthNode.rightParent.data).to.eql(3);
+    expect(p.root.rightChild.leftChild.data).to.eql(5);
+  });
 
   it('should be able to find a node');
 
@@ -27,11 +46,5 @@ describe('Pyramid',function(){
   it('should be able to find the path with the smallest sum of nodes');
 
   it('should be able to find the path with the largest sum of nodes');
-
-  // it('', function(){
-  //   prep(4,3);
-  //   stairs.solutions = stairs.recurse(stairs.stairs);
-  //   expect(stairs.solutions).to.eql(7);
-  // });
 
 });
--- src/pyramid/pyramid.js
+++ src/pyramid/pyramid.js
@@ -5,14 +5,45 @@ var PyramidNode = require('./pyramid-node');
 var Pyramid = function (data) {
   
   this.root = null;
+  this.inserted = 0;
+
+  if (data) generateFromArray(data)
 
 };
 
 Pyramid.prototype.generateFromArray = function (arr) {
-  for (var k = 0; k < arr.length - 1; k++) {
-    this.insertNextNumber(arr[k]);
+  var data = arr;
+  if (detectNested(data)) 
+    data = flatten(data);
+
+  for (var k = 0; k < data.length; k++) {
+    this.insertNextNumber(data[k]);
   }
+
   return true;
+
+  function detectNested (a) {
+    var nested = 0;
+    for (var j = 0; j < a.length; j++) {
+      if (typeof a[j] === 'object')
+        if (a[j].length === j + 1) nested++;
+        else; // not a full pyramid
+    }
+    if (nested === 0) return false;
+    if (nested === a.length) return true;
+    else throw new Error('Gived Pyramid data is incongruous.'); 
+  }
+
+  function flatten (a) {
+    var flattened = [];
+    for (var k = 0; k < a.length; k++) {
+      for (var j = 0; j < a[k].length; j++) {
+        flattened.push(a[k][j]);
+      }
+    }
+    return flattened;
+  }
+
 };
 
 Pyramid.prototype.insertNextNumber = function (number) {
@@ -23,50 +54,85 @@ Pyramid.prototype.insertNextNumber = function (number) {
     newNode.leftParent = false;
     newNode.rightParent = false;
     this.root = newNode;
+
+    this.inserted++;
+    // console.log('add ' + newNode.data + ' as root');
+
   } else {
+
     var current = this.root;
     insertLoop: while (true) {
 
       if (current.leftChild === null && current.rightChild === null) {
         newNode.rightParent = current;
         current.leftChild = newNode;
-        break insertLoop;
-      }
 
-      else if (current.leftChild !== null && current.rightChild === null) {
+        this.inserted++;
+        // console.log('add ' + newNode.data + ' as left child of ' + current.data);
+        break insertLoop;
+
+      } else if (current.leftChild !== null && current.rightChild === null) {
         newNode.leftParent = current;
-        current.rightChild == newNode;
-        // TODO check right parent ?
-        break insertLoop;
-      }
-
-      else if (current.leftChild !== null && current.rightChild !== null) {
-        // check for open slots to the right
+        current.rightChild = newNode;
+        this.inserted++;
+        // console.log('add ' + newNode.data + ' as right child of ' + current.data);
         
-        var rightSibling = current.rightParent.rightChild;
-
-        rightSiblingLoop: while (true) {
-          // check if current right sibling exists and has a right child
-          // NB: no need to check if right sibling has a *left child*
-          if (rightSibling.rightChild === null) {
-            rightSibling.rightChild = newNode;
-            break insertLoop;
-          }
-
-          // if right parent's right child exists, set that as the next right sibling
-          else if (!!rightSibling.rightParent && !!rightSibling.rightParent.rightChild){
-            rightSibling = rightSibling.rightParent.rightChild;
-            // loop again
-          }
-
-          else {
-            // no sibling slots open, look left of current
-            current = current.leftChild;
-            break rightSiblingLoop;
-          }
-
+        // if there is a right parent, set as left child of right parent
+        if (current.rightParent && current.rightParent.rightChild){
+          newNode.rightParent = current.rightParent.rightChild;
+          current.rightParent.rightChild.leftChild = newNode;
+        } else {
+          newNode.rightParent = false;
         }
-        
+
+        break insertLoop;
+
+      } else if (current.leftChild !== null && current.rightChild !== null) {
+        // check for open slots to the right
+        // console.log('checking right siblings');
+
+        var rightSibling = false;
+
+        if (current.rightParent && current.rightParent.rightChild) {
+
+          rightSibling = current.rightParent.rightChild;
+
+          rightSiblingLoop: while (true) {
+
+            // console.log('entering rightSiblingLoop');
+
+            // check if current right sibling exists and has a right child
+            // NB: no need to check if right sibling has a *left child*
+            if (rightSibling.rightChild === null) {
+              newNode.leftParent = rightSibling;
+              rightSibling.rightChild = newNode;
+              this.inserted++;
+              // console.log('add ' + newNode.data + ' as right child of ' + rightSibling.data);
+              break insertLoop;
+            }
+
+            // if right parent's right child exists, set that as the next right sibling
+            else if (!!rightSibling.rightParent && !!rightSibling.rightParent.rightChild){
+              rightSibling = rightSibling.rightParent.rightChild;
+
+              // console.log('looking for another right sibling');
+              // loop again
+            }
+
+            else {
+              // no sibling slots open, look left of current
+              current = current.leftChild;
+
+              // console.log('looking left');
+              break rightSiblingLoop;
+            }
+
+          }
+
+        } else {
+          current = current.leftChild;
+        }
+
       }
       
     }
